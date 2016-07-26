f21dd4e033b5910a1df696df64c23413d944769b
bjf2015 <bfranzoi@icloud.com>
2016-07-26 11:33:37 -0700

    Updated

--- main.js
+++ main.js
@@ -1,52 +1,98 @@
-var Pantry = [];
-var Pantry = {
-    strong: ['glugOfRum', 'slugOfWhiskey', 'splashOfGin'],
-    salty: ['oliveOnStick', 'saltDustedRim', 'baconRasher'],
-    bitter: ['bitters', 'splashOfTonic', 'lemonTwist'],
-    sweet: ['sugar', 'honey', 'cola'],
-    fruity: ['orangeSlice', 'cassis', 'cherry']
+var attributes = {
+    STRONG: 'STRONG',
+    SALTY: 'SALTY',
+    BITTER: 'BITTER',
+    SWEET: 'SWEET',
+    FRUITY: 'FRUITY'
+};
+
+function Bartender(myQuestionList) {
+    this.questionList = myQuestionList;
 }
 
+Bartender.prototype.createDrink = function() {
+    var order = new Order();
+    var questions = this.questionList.questions;
+    for (var i=0; i<questions.length; i++) {
+        var question = questions[i];
+        var preference = question.getPreference();
+        order.addPreference(preference);
+    }
+    return order;
+};
+
+var Question = function(attribute, text) {
+    this.attribute = attribute;
+    this.text = text;
+};
+
+Question.prototype.getPreference = function() {
+    // TODO: Find a better way to get access to the select
+    var select = $('.' + this.attribute);
+    var isLiked = select.val() === "true";
+    var preference = new Preference(this.attribute, isLiked);
+    return preference;
+};
+
+Question.prototype.asHTML = function() {
+    var select = (
+        '<select class="' + this.attribute + '">' +
+            '<option value="true">Aye</option>' +
+            '<option value="false">Nay</option>' +
+        '</select>'
+    );
+    return '<li>' + this.text + ' ' + select + '</li>';
+};
+
+var QuestionList = function() {
+    this.questions = [];
+};
+
+QuestionList.prototype.addQuestion = function(question) {
+    this.questions.push(question);
+};
+
+QuestionList.prototype.asHTML = function() {
+    return this.questions.reduce(function(prev, question) {
+        return prev + question.asHTML();
+    }, '');
+};
+
+var Order = function() {
+    this.preferences = {};
+};
+
+Order.prototype.addPreference = function(preference) {
+    this.preferences[preference.attribute] = preference;
+};
+
+var Preference = function(attribute, isLiked) {
+    this.attribute = attribute; // Whether it is strong, salty etc.
+    this.isLiked = isLiked;
+};
 
 $(document).ready(function() {
-        function prefCreator(strong, salty, bitter, sweet, fruity) {
-            this.strong = strong;
-            this.salty = salty;
-            this.bitter = bitter;
-            this.sweet = sweet;
-            this.fruity = fruity;
-        }
+    var questionList = new QuestionList();
 
-        function renderDrink(usedIngredient) {
-            var renderIngredient = '<div id="list-of-ingredients">' + usedIngredient + '</div>';
-            $('#list-of-ingredients').append(renderIngredient);
-        
-        }
+    var strongQuestion = new Question(attributes.STRONG, 'Do ye like yer drinks strong?');
+    var saltyQuestion = new Question(attributes.SALTY, 'Do ye like it with a salty tang?');
+    var bitterQuestion = new Question(attributes.BITTER, 'Are ye a lubber who likes it bitter?');
+    var sweetQuestion = new Question(attributes.SWEET, 'Would ye like a bit of sweetness with yer poison?');
+    var fruityQuestion = new Question(attributes.FRUITY, 'Are ye one for a fruity finish?');
 
+    questionList.addQuestion(strongQuestion);
+    questionList.addQuestion(saltyQuestion);
+    questionList.addQuestion(bitterQuestion);
+    questionList.addQuestion(sweetQuestion);
+    questionList.addQuestion(fruityQuestion);
 
-        $('#button').click(function(event) {
-                var Drink = {};
-                event.preventDefault();
-                var preferences = new prefCreator(
-                    parseInt($('input[name="strong"]:checked').val()),
-                    parseInt($('input[name="salty"]:checked').val()),
-                    parseInt($('input[name="bitter"]:checked').val()),
-                    parseInt($('input[name="sweet"]:checked').val()),
-                    parseInt($('input[name="fruity"]:checked').val()));
-                console.log(preferences);
-                for (var key in preferences) {
-                    console.log(key);
-                    console.log(preferences[key]);
-                    if (preferences[key]) {
-                        var usedIngredient = Pantry[key][Math.floor(Math.random() * 3)];
-                        console.log(Drink);
-                 renderDrink(usedIngredient);   
-                    }
+    var bartender = new Bartender(questionList);    
 
-            }
-                
-        });
-        
+    $('.question-list').html(bartender.questionList.asHTML());
 
+    $('.place-order').click(function() {
+        var order = bartender.createDrink();
+    $('#output').asHTML(order);
+    });
 
 });
