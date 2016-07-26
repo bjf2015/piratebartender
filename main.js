8f704756598ebb34a0997104fedc32469f374c4c
bjf2015 <bfranzoi@icloud.com>
2016-07-26 09:39:22 -0700

    HTML appears

--- main.js
+++ main.js
@@ -7,39 +7,46 @@ var Pantry = {
     fruity: ['orangeSlice', 'cassis', 'cherry']
 }
 
-	
+
 $(document).ready(function() {
-    function prefCreator(strong, salty, bitter, sweet, fruity) {
-        this.strong = strong;
-        this.salty = salty;
-        this.bitter = bitter;
-        this.sweet = sweet;
-        this.fruity = fruity;
-    }
-    $('#button').click(function(event) {
-        var Drink = {};
-        event.preventDefault();
-        var preferences = new prefCreator(
-            parseInt($('input[name="strong"]:checked').val()),
-            parseInt($('input[name="salty"]:checked').val()),
-            parseInt($('input[name="bitter"]:checked').val()),
-            parseInt($('input[name="sweet"]:checked').val()),
-            parseInt($('input[name="fruity"]:checked').val()));
-        console.log(preferences);
-        for (var key in preferences){
-        	console.log(key);
-        	console.log(preferences[key]);
-        	if (preferences[key]) {
-        		Drink[key] = Pantry[key][Math.floor(Math.random() * 3)];
-        		console.log(Drink);
-
+        function prefCreator(strong, salty, bitter, sweet, fruity) {
+            this.strong = strong;
+            this.salty = salty;
+            this.bitter = bitter;
+            this.sweet = sweet;
+            this.fruity = fruity;
         }
 
-        	
+        function renderDrink(usedIngredient) {
+            var renderIngredient = '<div id="list-of-ingredients">' + usedIngredient + '</div>';
+            $('#list-of-ingredients').append(renderIngredient);
+        
         }
+
+
+        $('#button').click(function(event) {
+                var Drink = {};
+                event.preventDefault();
+                var preferences = new prefCreator(
+                    parseInt($('input[name="strong"]:checked').val()),
+                    parseInt($('input[name="salty"]:checked').val()),
+                    parseInt($('input[name="bitter"]:checked').val()),
+                    parseInt($('input[name="sweet"]:checked').val()),
+                    parseInt($('input[name="fruity"]:checked').val()));
+                console.log(preferences);
+                for (var key in preferences) {
+                    console.log(key);
+                    console.log(preferences[key]);
+                    if (preferences[key]) {
+                        var usedIngredient = Pantry[key][Math.floor(Math.random() * 3)];
+                        console.log(Drink);
+                 renderDrink(usedIngredient);   
+                    }
+
+            }
+                
+        });
         
 
 
-    })
 });
-
