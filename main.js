d398bdd52b5251354af0ed0d2e401d52a6a42ed5
bjf2015 <bfranzoi@icloud.com>
2016-07-25 19:17:58 -0700

    Inputs received and Ingredients selected

--- main.js
+++ main.js
@@ -1,29 +1,43 @@
 var Pantry = [];
 var Pantry = {
-  strongIngredients: ['glugOfRum', 'slugOfWhiskey', 'splashOfGin'],
-  saltyIngredients: ['oliveOnStick', 'saltDustedRim', 'baconRasher'],
-  bitterIngredients: ['bitters', 'splashOfTonic', 'lemonTwist'],
-  sweetIngredients: ['sugar', 'honey', 'cola'],
-  fruityIngredients: ['orangeSlice', 'cassis', 'cherry']
+    strong: ['glugOfRum', 'slugOfWhiskey', 'splashOfGin'],
+    salty: ['oliveOnStick', 'saltDustedRim', 'baconRasher'],
+    bitter: ['bitters', 'splashOfTonic', 'lemonTwist'],
+    sweet: ['sugar', 'honey', 'cola'],
+    fruity: ['orangeSlice', 'cassis', 'cherry']
 }
 
+	
 $(document).ready(function() {
-      function prefCreator(strong, salty, bitter, sweet, fruity) {
+    function prefCreator(strong, salty, bitter, sweet, fruity) {
         this.strong = strong;
         this.salty = salty;
         this.bitter = bitter;
         this.sweet = sweet;
         this.fruity = fruity;
     }
-$('form').submit(function(event) {
-    event.preventDefault();
-    var preferences = new prefCreator(
-     parseInt($('input[name="strong"]:checked').val()),
-     parseInt($('input[name="salty"]:checked').val()),
-     parseInt($('input[name="bitter"]:checked').val()),
-     parseInt($('input[name="sweet"]:checked').val()),
-     parseInt($('input[name="fruity"]:checked').val()));
+    $('#button').click(function(event) {
+        var Drink = {};
+        event.preventDefault();
+        var preferences = new prefCreator(
+            parseInt($('input[name="strong"]:checked').val()),
+            parseInt($('input[name="salty"]:checked').val()),
+            parseInt($('input[name="bitter"]:checked').val()),
+            parseInt($('input[name="sweet"]:checked').val()),
+            parseInt($('input[name="fruity"]:checked').val()));
+        console.log(preferences);
+        for (var key in preferences){
+        	console.log(key);
+        	console.log(preferences[key]);
+        	if (preferences[key]) {
+        		Drink[key] = Pantry[key][Math.floor(Math.random() * 3)];
+        		console.log(Drink);
 
+        }
+
+        	
+        }
+        
 
 
     })
